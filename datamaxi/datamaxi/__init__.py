9d93990ec2208b36ea9dc1cd5e4704b613ec31d9
Martin Kersner <martin@bisonai.com>
2024-07-09 13:30:25 +0900

    Pagination (#41)
    * feat: pagination
    
    * feat: convert lambda to def
    
    * chore: remove binance funding rate test
    
    * chore: bump up version

--- datamaxi/datamaxi/__init__.py
+++ datamaxi/datamaxi/__init__.py
@@ -1,9 +1,8 @@
-from typing import Any, List, Union
+from typing import Any, Callable, Tuple, List, Dict, Union
 import pandas as pd
 from datamaxi.api import API
 from datamaxi.lib.utils import check_required_parameter
 from datamaxi.lib.utils import check_required_parameters
-from datamaxi.lib.utils import postprocess
 from datamaxi.lib.constants import BASE_URL
 
 
@@ -58,15 +57,19 @@ class Datamaxi(API):
         url_path = "/v1/intervals"
         return self.query(url_path, params)
 
-    @postprocess()
     def candle(
         self,
         exchange: str,
         symbol: str,
         interval: str = "1d",
         market: str = "spot",
+        page: int = 1,
+        limit: int = 1000,
+        fromDateTime: str = None,
+        toDateTime: str = None,
+        sort: str = "desc",
         pandas: bool = True,
-    ) -> Union[List, pd.DataFrame]:
+    ) -> Union[Tuple[Dict, Callable], Tuple[pd.DataFrame, Callable]]:
         """Get candle data
 
         `GET /v1/candle`
@@ -78,10 +81,15 @@ class Datamaxi(API):
             symbol (str): Symbol name
             interval (str): Candle interval
             market (str): Market type (spot/futures)
+            page (int): Page number
+            limit (int): Limit of data
+            fromDateTime (str): Start date and time (accepts format "2006-01-02 15:04:05" or "2006-01-02")
+            toDateTime (str): End date and time (accepts format "2006-01-02 15:04:05" or "2006-01-02")
+            sort (str): Sort order
             pandas (bool): Return data as pandas DataFrame
 
         Returns:
-            Candle data for a given symbol, interval and market in pandas DataFrame
+            Candle data for a given symbol, interval and market in pandas DataFrame and next request function
         """
         check_required_parameters(
             [
@@ -95,10 +103,55 @@ class Datamaxi(API):
         if market not in ["spot", "futures"]:
             raise ValueError("market must be either spot or futures")
 
+        if page < 1:
+            raise ValueError("page must be greater than 0")
+
+        if limit < 1:
+            raise ValueError("limit must be greater than 0")
+
+        if fromDateTime is not None and toDateTime is not None:
+            raise ValueError(
+                "fromDateTime and toDateTime cannot be set at the same time"
+            )
+
+        if sort not in ["asc", "desc"]:
+            raise ValueError("sort must be either asc or desc")
+
         params = {
             "exchange": exchange,
             "symbol": symbol,
             "interval": interval,
             "market": market,
+            "page": page,
+            "limit": limit,
+            "fromDateTime": fromDateTime,
+            "toDateTime": toDateTime,
+            "sort": sort,
         }
-        return self.query("/v1/candle", params)
+
+        res = self.query("/v1/candle", params)
+        if res["data"] is None:
+            raise ValueError("no data found")
+
+        def next_request():
+            return self.candle(
+                exchange,
+                symbol,
+                interval,
+                market,
+                page + 1,
+                limit,
+                fromDateTime,
+                toDateTime,
+                sort,
+                pandas,
+            )
+
+        if pandas:
+            df = pd.DataFrame(res["data"])
+            df = df.set_index("d")
+            df.replace("NaN", pd.NA, inplace=True)
+            df = df.apply(pd.to_numeric, errors="coerce")
+            return df, next_request
+        else:
+            return res, next_request
